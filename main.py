71a0115007f1d8b1742487617a99f55b0c964ef7
WitherWarg <ykhemiss@gmail.com>
2025-07-31 13:09:56 -0400

    Added file reader

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from sample_ai import generate_prompt
+from google.genai import Client
+from pathlib import Path
+
+client = Client()
+model = "gemini-2.5-pro"
+
+folder_path = "./Sample"
+folder = Path(folder_path)
+files = []
+
+for file in folder.rglob('*'):
+    if file.is_file():
+        files.append(str(file))
+        files.append(client.files.upload(file=str(file)))
 
 while True:
     try:
-        message = input("Chat:\n\t")
+        prompt = input("Prompt: ")
     except KeyboardInterrupt:
         print()
         break
 
-    print(generate_prompt(message))
+    contents = [
+        prompt,
+        '''
+        The user will ask you questions about the contents of these files.
+        You will summarize the contents and ONLY the contents relating to
+        the user's query in two to three sentences. Do not use transition
+        terms such as "Based on the report" or "According to the projet".
+        Instead, get right into the subject right away.
+        '''
+    ]
+    contents.extend(files)
+    print(contents)
+
+    # print(client.models.generate_content(model=model, contents=contents).text)
--- sample_ai.py
+++
@@ -1,6 +0,0 @@
-from google import genai
-
-def generate_prompt(contents):
-    return genai.Client().models.generate_content(
-        model="gemini-2.5-flash", contents=contents
-    ).text
